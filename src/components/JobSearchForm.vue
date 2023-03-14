62b483220e0f765351c174281db0bab9e03913c7
Srdjan Stevic <srdjan.stevic@vegait.rs>
2023-03-14 16:24:25 +0100

    the hero section job-search-form css stayling

---
+++ src/components/JobSearchForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <form
+    class="flex h-12 w-full items-center rounded-3xl border border-solid border-brand-gray-3"
+  >
+    <font-awesome-icon :icon="['fas', 'search']" class="ml-4 mr-3" />
+
+    <div class="flex h-full flex-1 flex-nowrap text-base font-light">
+      <div class="relative flex h-full flex-1 items-center pr-3">
+        <label class="absolute left-0 -top-10">Role</label>
+        <input
+          type="text"
+          class="w-full text-lg font-normal focus:outline-none"
+        />
+      </div>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "JobSearchForm",
+};
+</script>
